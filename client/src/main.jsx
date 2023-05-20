937d45593ebdbd7634567fc1981c30307bfc428d
Igor Szalas <igorszalas@int.pl>
2023-05-20 20:08:13 +0200

    Improve React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
