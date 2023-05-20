23b4e5cb5e84135b9c534f869763c540286bdf37
Igor Szalas <igorszalas@int.pl>
2023-05-20 20:52:49 +0200

    Fix problems with keys in courses

--- client/src/components/CoursesSection.jsx
+++ client/src/components/CoursesSection.jsx
@@ -13,27 +13,30 @@ const CoursesSection = () => {
         }}
       >
         CoursesSection
-        {dummyCourses.map(({ courseTitle, courseDescripton, courseImage }) => (
-          <Card
-            style={{
-              padding: "3rem",
-              margin: "3rem",
-              minWidth: "60%",
-              maxWidth: "50%",
-            }}
-          >
-            <Card.Img
-              variant="top"
-              style={{ maxHeight: "8rem", maxWidth: "8rem" }}
-              src={courseImage}
-            />
-            <Card.Body>
-              <Card.Title>{courseTitle}</Card.Title>
-              <Card.Text>{courseDescripton}</Card.Text>
-              <Button variant="primary">Zapisz się na kurs!</Button>
-            </Card.Body>
-          </Card>
-        ))}
+        {dummyCourses.map(
+          ({ courseTitle, courseDescripton, courseImage, courseID }) => (
+            <Card
+              style={{
+                padding: "3rem",
+                margin: "3rem",
+                minWidth: "60%",
+                maxWidth: "50%",
+              }}
+              key={courseID}
+            >
+              <Card.Img
+                variant="top"
+                style={{ maxHeight: "8rem", maxWidth: "8rem" }}
+                src={courseImage}
+              />
+              <Card.Body>
+                <Card.Title>{courseTitle}</Card.Title>
+                <Card.Text>{courseDescripton}</Card.Text>
+                <Button variant="primary">Zapisz się na kurs!</Button>
+              </Card.Body>
+            </Card>
+          )
+        )}
       </div>
     </>
   );
