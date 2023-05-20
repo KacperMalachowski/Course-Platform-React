ec1b15fc8c55a4a03cd61068cbe2d8706dabfaac
Igor Szalas <igorszalas@int.pl>
2023-05-20 15:06:48 +0200

    Add better email and password fields

--- client/src/components/NewCourseForm.jsx
+++ client/src/components/NewCourseForm.jsx
@@ -2,20 +2,10 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
-<<<<<<< HEAD
-=======
-
->>>>>>> new-course-form
 const NewCourseForm = () => {
   return (
     <>
       NewCourseForm
-<<<<<<< HEAD
-      <Card>
-        <Form className="mb-3">
-          <FloatingLabel label="Nazwa kursu" className="mb-3">
-            <Form.Control as="textarea" placeholder="Opis kursu" />
-=======
       <Card style={{ padding: "5rem", margin: "5rem" }}>
         <Form className="mb-3">
           <Form.Label>
@@ -28,26 +18,17 @@ const NewCourseForm = () => {
           </Form.Text>
           <FloatingLabel label="Nazwa kursu" className="mb-3">
             <Form.Control placeholder="Nazwa kursu" />
->>>>>>> new-course-form
           </FloatingLabel>
           <FloatingLabel className="mb-3" label="Opis kursu">
             <Form.Control
               as="textarea"
               placeholder="Opis kursu"
-<<<<<<< HEAD
-              style={{ height: "100px" }}
-            />
-          </FloatingLabel>
-          <Button className="mb-3" variant="primary" type="submit">
-            Submit
-=======
               maxLength="2200"
               style={{ height: "100px", maxHeight: "400px" }}
             />
           </FloatingLabel>
           <Button className="mb-3" variant="primary" type="submit">
             Dodaj nowy kurs!
->>>>>>> new-course-form
           </Button>
         </Form>
       </Card>
